11c598c87797940f1e734c0bd1cc6017b33dd5b9
maxrumbo <maxrumbo06@gmail.com>
2025-07-03 00:16:24 +0700

    ya

--- PEMPROS-maxrumbo/2425-ge-t11-file-operation-maxrumbo/libs/gender.c
+++
@@ -1,7 +0,0 @@
-#include "gender.h"
-
-/**
- * @brief Define the complete function definition here. Be sure to enlist the prototype of each function
- * defined here in the corresponding header file.
- *
- */
